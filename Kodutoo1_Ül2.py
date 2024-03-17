db8793a6b2757ddd84b54aead92023ee2ac225cc
OliverUtsar <163525886+OliverUtsar@users.noreply.github.com>
2024-03-17 22:26:53 +0200

    Add files via upload

---
+++ Kodutoo1_Ül2.py
@@ -0,0 +1,13 @@
+class Solution:
+        #defineerisin funktsiooni, mis teisendab Celsiuse väärtuse Kelviniks ning Fahrenheitiks
+        def convertTemperature(self, Celsius):
+            Kelvin = Celsius + 273.15
+            Fahrenheit = Celsius * 1.80 + 32.00
+            #Lõin tühja listi
+            convertTemperature = []
+            #Tühja listi lisan Fahrenheiti ning Kelvini väärtused
+            convertTemperature = [Kelvin, Fahrenheit]
+            #Tagastan teisendatud väärtused
+            return convertTemperature
+#prindin välja väärtused, mil Celsius on 36.50
+print(Solution().convertTemperature(36.50))
