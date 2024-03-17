a7d3acd96a7fd936a930e8a0fcc590ee00325a34
OliverUtsar <163525886+OliverUtsar@users.noreply.github.com>
2024-03-17 22:30:15 +0200

    Update Kodutoo1_Ül3.py

--- Kodutoo1_Ül3.py
+++ Kodutoo1_Ül3.py
@@ -1,4 +1,7 @@
 class Solution:
+    #defineerisin funktsiooni, mis võtab sisendiks esimese arvu ning teise ning liidab arvud kokku
     def sum(self, num1, num2):
+        #liidab esimese ja teise arvu omavahel
         sum = num1 + num2
+        #väljastab kahe arvu summa
         return sum
