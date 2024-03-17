182c9b25557066457606bc6a852366b41db1a81a
pebchh <163525648+pebchh@users.noreply.github.com>
2024-03-17 23:04:20 +0200

    Delete Kodutoo1_Ül3.py

--- Kodutoo1_Ül3.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    #defineerisin funktsiooni, mis võtab sisendiks esimese arvu ning teise ning liidab arvud kokku
-    def sum(self, num1, num2):
-        #liidab esimese ja teise arvu omavahel
-        sum = num1 + num2
-        #tagastab kahe arvu summa
-        return sum
