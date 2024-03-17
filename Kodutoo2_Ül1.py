30ba5d54df05f3040da8ce4521b4deaace6c98b9
OliverUtsar <163525886+OliverUtsar@users.noreply.github.com>
2024-03-17 23:06:29 +0200

    Update Kodutoo2_Ül1.py

--- Kodutoo2_Ül1.py
+++ Kodutoo2_Ül1.py
@@ -1,3 +1,6 @@
+https://leetcode.com/problems/defanging-an-ip-address/description/
 class Solution:
+    #defineerisin funktsiooni defangIPaddr
     def defangIPaddr(self, address):
+        #asendab aadressis punktid [.] tähistusega ning tagastab aadressi sellisel kujul
         return address.replace(".", "[.]") 
