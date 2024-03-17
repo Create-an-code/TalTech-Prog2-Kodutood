9232e851ae0c9df5775d2e5edf351dbdc3c540cb
OliverUtsar <163525886+OliverUtsar@users.noreply.github.com>
2024-03-17 22:26:25 +0200

    Delete Kodutoo1_Ül2.py

--- Kodutoo1_Ül2.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-        def convertTemperature(self, Celsius):
-            Kelvin = Celsius + 273.15
-            Fahrenheit = Celsius * 1.80 + 32.00
-            convertTemperature = []
-            convertTemperature = [Kelvin, Fahrenheit]
-            return convertTemperature
-print(Solution().convertTemperature(36.50))
